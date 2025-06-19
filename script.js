79f9b28a845b856d0c1971a7cfae702043178fe2
kbkhan594@email.com <kbkhan594@email.com>
2025-06-19 17:15:28 +0530

    last Commit

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ counters.forEach((counter)  => {
    counter.innerHTML = 0;
 
   const updateCounter = () => {
-    const targetcount = counter.getAttribute('data-target');
-    console.log(targetcount);
+    const targetcount = +counter.getAttribute('data-target');
+    //console.log(typeof targetcount);
+
+    const startingCount =  Number(counter.innerHTML);
+
+    const increment = targetcount / 100;
+
+    if (startingCount < targetcount){
+     counter.innerHTML = `${Math.round(startingCount + increment)}`;
+     setTimeout(updateCounter , 100)
+    }else{
+       counter.innerHTML =  targetcount;
+    }
+
   }
 
-});
+  updateCounter();
+})
